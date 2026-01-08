0e3ed5da4cac68432c2a421e06fc64fcbcb67107
DoppelGinger <mr.tachibana@mail.ru>
2026-01-09 01:01:02 +0500

    initial

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './dist/',
+  base: './dist/2',
   plugins: [
     react(),
     tailwindcss(),
